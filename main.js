b8f30e9033bcba3775e9266724f0316cc45d3cde
Emmanuel-002 <segseyman8@gmail.com>
2024-07-16 17:21:55 +0100

    edited the about page

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed = new Typed('#element', {
-    strings: ['<i>Fullstack Developer</i>', ' <i>MangoDb</i>', ' <i>Epress JS</i>', '<i>React JS</i>', '<i>Node JS</i>'],
+    strings: [' <i>MangoDb</i>', ' <i>Epress JS</i>', '<i>React JS</i>', '<i>Node JS</i>', 'HTML 5', 'CSS 3', 'Material UI', 'Bootstrap', 'Github', 'Linux', 'Algortithms', 'Data Structures'],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
